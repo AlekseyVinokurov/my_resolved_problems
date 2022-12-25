eafcff7fa389c428a49cedd5f634093ccb9abc6d
AlekseyVinokurov <agat.prof@gmail.com>
2022-12-25 21:49:59 +0500

    Commit Massage

--- december_22.py
+++ december_22.py
@@ -268,4 +268,20 @@ s = "   fly me   to   the moon  "
 print(Solution().lengthOfLastWord(s))
 # Output: 4
 
+'''
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+'''
+class Solution(object):
+    def climbStairs(self, n):
+        a = b = 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
+n = 2
+print(Solution().climbStairs(n))
+# Output: 2
+n = 3
+print(Solution().climbStairs(n))
+# Output: 3
 
