390a990e21fd0b29e4f30d18583a456c5c4b81c8
AlekseyVinokurov <agat.prof@gmail.com>
2022-12-30 16:18:32 +0500

    Commit Massage

--- december_22.py
+++ december_22.py
@@ -491,3 +491,33 @@ for i in range(1, int(input())+1):
         print(num, end=' ')
     print()
 
+'''
+Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node 
+n - 1 and return them in any order.
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed 
+edge from node i to node graph[i][j]).
+'''
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        def back_tracker(node, path):
+            if node == len(graph) - 1:
+                res.append(path)
+                return
+            for nei in graph[node]:
+                back_tracker(nei, path + [nei])
+        res = []
+        back_tracker(0, [0])
+        return res
+graph = [[1,2],[3],[3],[]]
+print(Solution().allPathsSourceTarget(graph))
+# Output: [[0,1,3],[0,2,3]]
+graph = [[4,3,1],[3,2,4],[3],[4],[]]
+print(Solution().allPathsSourceTarget(graph))
+# Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+# Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+
+
