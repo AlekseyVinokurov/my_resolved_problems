bac19b602cc3edbb2286f05bf9bcc78c65b2f738
AlekseyVinokurov <agat.prof@gmail.com>
2023-01-08 21:59:58 +0500

    Commit Massage

--- januar_23.py
+++ januar_23.py
@@ -215,3 +215,33 @@ class Solution:
 
                 curr_wer = 0
         return starting_station if total_wer >= 0 else -1
+
+'''
+Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+'''
+from typing import List
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) <= 2:
+            return len(points)
+        res = 0
+        for i in range(len(points)):
+            d = {}
+            same = 1
+            for j in range(i + 1, len(points)):
+                if points[i][0] == points[j][0] and points[i][1] == points[j][1]:
+                    same += 1
+                elif points[i][0] == points[j][0]:
+                    d['inf'] = d.get('inf', 0) + 1
+                else:
+                    k = (points[i][1] - points[j][1]) / (points[i][0] - points[j][0])
+                    d[k] = d.get(k, 0) + 1
+            res = max(res, same + (max(d.values()) if d else 0))
+        return res
+
+points = [[1,1],[2,2],[3,3]]
+print(Solution().maxPoints(points))
+#Output: 3
+points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+print(Solution().maxPoints(points))
+#Output: 4
