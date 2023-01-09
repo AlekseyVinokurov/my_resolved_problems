70d80c18aa1a85c51ee3e4191dc1073750c021a1
AlekseyVinokurov <agat.prof@gmail.com>
2023-01-09 13:22:12 +0500

    Commit Massage

--- januar_23.py
+++ januar_23.py
@@ -245,3 +245,30 @@ print(Solution().maxPoints(points))
 points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
 print(Solution().maxPoints(points))
 #Output: 4
+
+'''
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+'''
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) <= 1:
+            return 0
+        min_price = prices[0]
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                max_profit = max(max_profit, prices[i] - min_price)
+        return max_profit
+prices = [7,1,5,3,6,4]
+print(Solution().maxProfit(prices))
+#Output: 5
