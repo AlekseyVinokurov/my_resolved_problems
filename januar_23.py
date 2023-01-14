99a3f3f0475b19d4288476542ce1eb56534597fb
AlekseyVinokurov <agat.prof@gmail.com>
2023-01-14 13:36:59 +0500

    Commit Massage

--- januar_23.py
+++ januar_23.py
@@ -521,3 +521,20 @@ class Solution(object):
         digits.append(0)
         digits[0] = 1
         return digits
+
+'''
+
+'''
+
+class Solution:
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+nums = [1,1,2]
+print(Solution().removeDuplicates(nums))
